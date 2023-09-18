471d2f304c20618cebbee3478b7c157b17b9c242
antcardia <antcardia4@alum.us.es>
2023-09-18 11:34:19 +0200

    Inicio del proyecto

---
+++ src/main/java/tfg/backend/scuttle/ScuttleApplication.java
@@ -0,0 +1,13 @@
+package tfg.backend.scuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScuttleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfg/backend/scuttle/ScuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package tfg.backend.scuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
