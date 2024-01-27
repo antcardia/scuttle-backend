dce86c8767e813f629136f5944f84caab8dbf213
antcardia <antcardia4@alum.us.es>
2024-01-27 16:25:56 +0100

    HTTPS habilitado

--- src/main/java/tfg/backend/scuttle/configuration/WebConfiguration.java
+++ src/main/java/tfg/backend/scuttle/configuration/WebConfiguration.java
@@ -20,7 +20,7 @@ public class WebConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("127.0.0.1:5173");
+        config.addAllowedOrigin("https://scuttle.vercel.app");
         config.setAllowedHeaders(Arrays.asList(
             HttpHeaders.AUTHORIZATION,
             HttpHeaders.CONTENT_TYPE,
